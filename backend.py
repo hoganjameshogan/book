97d5217fb08560a210f20b41be2985f9b2bea732
James Hogan <jameshoganobair@gmail.com>
2021-02-06 20:49:08 +0100

    update

--- backend.py
+++ backend.py
@@ -2,60 +2,49 @@ import sqlite3 as sq3
 
 class Database:
     def __init__(self, db):
-        conn = sq3.connect(db)
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
-        conn.commit()
-        conn.close()
+        self.conn = sq3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        self.conn.commit()
+        # conn.close()
 
     def insert(self,title,author,year,isbn):
-        conn = sq3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO books VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
-        conn.commit()
+        self.cur.execute("INSERT INTO books VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
+        self.conn.commit()
         print("back --> Inserted : {}-{}-{}-{}".format(title,author,year,isbn))
-        conn.close()
 
     def view(self):
-        conn = sq3.connect("books.db")
-        cur = conn.cursor()
-        c = cur.execute("SELECT * FROM books").fetchall()
-        conn.commit()
-        conn.close()
+        c = self.cur.execute("SELECT * FROM books").fetchall()
+        self.conn.commit()
         return c
 
     def update(self,id,title,author,year,isbn):
         print("Updating with {},{},{},{}".format(title,author,year,isbn))
-        conn = sq3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("UPDATE books SET \
+        self.cur.execute("UPDATE books SET \
             title = ? , \
             author = ? , \
             year = ? , \
             isbn = ? \
             WHERE id = ?" , (title,author,year,isbn,id))
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
     def search(self,title="",author="",year="",isbn=""):
-        conn = sq3.connect("books.db")
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         c = cur.execute("SELECT * FROM books WHERE \
             title=? OR \
             author=? OR \
             year=? OR \
             isbn=? \
             ",(title,author,year,isbn)).fetchall()
-        conn.commit()
-        conn.close()
+        self.conn.commit()
         return c
 
     def delete(self,id):
-        conn = sq3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("DELETE FROM books WHERE id=?", (id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("DELETE FROM books WHERE id=?", (id,))
+        self.conn.commit()
+
+    def del(self):
+        self.conn.close()
 
 
 
